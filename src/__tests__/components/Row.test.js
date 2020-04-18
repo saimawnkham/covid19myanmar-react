3c6aa0c01b28c4492e0b07ca3243971a35917662
Thant Thet <t@ttkz.me>
2020-04-18 23:43:14 +0630

    Update row test

--- src/__tests__/components/Row.test.js
+++ src/__tests__/components/Row.test.js
@@ -59,7 +59,7 @@ describe('Row component', () => {
 
     expect(stateSelector).toHaveLength(1);
     expect(cells).toHaveLength(5);
-    expect(stateName).toContain(state.state);
+    expect(stateName).toContain(state.state.toLowerCase());
     expect(confirmed).toEqual('11');
     expect(active).toEqual('1');
     expect(recovered).toEqual('10');
